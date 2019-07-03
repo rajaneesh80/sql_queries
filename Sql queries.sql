289ea95e0fd116834b66ad3869ab92e0bee095cf
rajneesh <rajneesh@yumi.co.uk>
2019-07-03 17:24:52 +0100

    left jion added

--- Sql queries.sql
+++ Sql queries.sql
@@ -21,7 +21,7 @@ ON item.CustOrderRSN_ID = head.Id
 AND item.sku_id = sku.Id 
 AND head.Customer_Id = customer.Id 
 
-AND cus_code = 'UABB' 
+AND cus_code = 'DRES4' 
 
 GROUP BY SKU 
 ORDER BY SKU DESC
@@ -545,18 +545,21 @@ And
 AND 
 	head.Customer_Id = customer.Id 
 AND
-	report_stock.Avilable_Stock > '0'
+	report_stock.Avilable_Stock > '2'
 AND 
-	cus_code = 'DRES4' 
-    
+	cus_code = 'ZALANL 3'
+/*
 WHERE report_stock.BrandName IN ('YUMI RETAIL','YUMI ORIGINAL', 'YUMI GIRLS RETAIL', 'YUMI GIRLS', 'Yumi Curves',
 								'YUMI ACCESSORIES', 'YUMI' ,'Y by Yumi', 'UTTAM KIDS', 'ISKA', 'YUMI-RETAIL') AND 
+*/
+WHERE report_stock.BrandName IN ('ISKA', 'MELA', 'MELA LONDON') AND 
+                                
 report_stock.Season IN ('AW10', 'AW11', 'AW12', 'AW13', 'AW14', 'AW15', 
-'AW16', 'AW17', 'AW18', 'IS10', 'IS11', 
+'AW16', 'AW17', 'AW18', 'AW19','IS10', 'IS11', 
 'IS12', 'IS13', 'IS14', 'IS15', 'IW10', 'IW11', 
 'IW12', 'IW13', 'IW14', 'IW15', 'PD19', 'SS01', 
 'SS02', 'SS10', 'SS11', 'SS12', 'SS13', 'SS14', 
-'SS15', 'SS16', 'SS17', 'SS18', 'SS19', 'Missing')
+'SS15', 'SS16', 'SS17', 'SS18', 'SS19', 'SS20','Missing')
 
 GROUP BY SKU 
 ORDER BY SKU DESC
@@ -634,38 +637,245 @@ ORDER BY SKU_ID DESC
 
 //////////////////////////////// Pivot  ///////////////
 
-create view report_stock_Extended as (
+create view report_stock_Extended_AW17 as (
   select
     report_stock.BrandName,
     case when Season = "SS19" then Avilable_Stock end as SS19,
     case when Season = "SS18" then Avilable_Stock end as SS18,
-    case when Season = "SS17" then Avilable_Stock end as SS17
+    case when Season = "SS17" then Avilable_Stock end as SS17,
+    case when Season = "AW19" then Avilable_Stock end as AW19,
+    case when Season = "AW18" then Avilable_Stock end as AW18,
+    case when Season = "AW17" then Avilable_Stock end as AW17
    
   from report_stock
 );
 
-create view report_stock_Extended_Pivot as (
+create view report_stock_Extended_Pivot_AW17 as (
   select
     BrandName,
     sum(SS19) as SS19,
     sum(SS18) as SS18,
-    sum(SS17) as SS17 
-  from report_stock_Extended
+    sum(SS17) as SS17,
+	sum(AW19) as AW19,
+    sum(AW18) as AW18,
+    sum(AW17) as AW17
+  from report_stock_Extended_Aw17
   group by BrandName
 );
 
-create view report_stock_Extended_Pivot_Pretty as (
+create view report_stock_Extended_Pivot_Pretty_AW17 as (
   select 
     BrandName, 
     coalesce(SS19, 0) as SS19, 
     coalesce(SS18, 0) as SS18, 
-    coalesce(SS17, 0) as SS17
-  from report_stock_Extended_Pivot
+    coalesce(SS17, 0) as SS17,
+	coalesce(AW19, 0) as AW19, 
+    coalesce(AW18, 0) as AW18, 
+    coalesce(AW17, 0) as AW17
+  from report_stock_Extended_Pivot_AW17
 );
 
-//////////////////////////////// end   ///////////////
+/*/////////////////////////////// end   //////////////*/
 
+create view report_stock_Extended_SS19 as (
+  select
+    report_stock.BrandName,
+    case when Season = "SS19" then Avilable_Stock end as SS19
+    from report_stock
+);
 
+create view report_stock_Extended_Pivot_SS19 as (
+  select
+    BrandName,
+    sum(SS19) as SS19
+    from report_stock_Extended
+  group by BrandName
+);
+
+create view report_stock_Extended_Pivot_Pretty_SS19 as (
+  select 
+    BrandName, 
+    coalesce(SS19, 0) as SS19
+    from report_stock_Extended_Pivot
+);
+
+/*/////////////////////////////// end   //////////////*/
+
+########### Customer Order Sold Report_ for _Offer ####################
+
+SELECT
+sku.LineCol, sku.Style, item.ColourCode, sku.Colour,
+sku.BrandName, sku.Season,
+item.sku_id,
+min(item.SellPrice) as Lowest_Sold_Price,
+sum(item.TotalQty) as Total_Qty_Sold, count(head.RSN) as Tiems_Sold,
+item.BuyPrice as Landed_Cost_GBP,
+max(item.SellPrice) as Max_Sold_Price, 
+customer.Cus_Code as Customer_Code, customer.Name as Customer_Name,
+head.Status,
+item.Article
+
+FROM item join head join customer join sku 
+
+ON item.CustOrderRSN_ID = head.Id 
+AND item.sku_id = sku.Id 
+AND head.Customer_Id = customer.Id 
+
+AND cus_code = 'DRES4' 
+
+GROUP BY sku_id 
+ORDER BY sku_id DESC
+
+/*/////////////////////////////// end   //////////////*/
+
+########### Customer Order Sold Report by Customer ####################
+
+SELECT
+report_stock.LineCol, report_stock.Style, report_stock.Colour,
+report_stock.BrandName, report_stock.Season, report_stock.RRPGBP,
+report_stock.RRPEuro, report_stock.Avilable_Stock, report_stock.Count,
+report_stock.Collection, report_stock.Article,
+
+customer_report_stock.Lowest_Sold_Price, customer_report_stock.Total_Qty_Sold,
+customer_report_stock.Tiems_Sold, customer_report_stock.Max_Sold_Price,
+customer_report_stock.Customer_Code,
+
+sku.Origin, sku.FabCom, sku.WashCare
+
+FROM customer_report_stock join report_stock join sku
+
+ON report_stock.SKU_Id = sku.Id
+
+AND customer_report_stock.Report_Stock_Id = report_stock.Id
+
+GROUP BY LineCol 
+ORDER BY LineCol
+
+/*/////////////////////////////// end   //////////////*/
+
+########### Customer Order Sold Report by Customer _1.0 ####################
+
+SELECT
+report_stock.LineCol, report_stock.Style, report_stock.Colour,
+report_stock.BrandName, report_stock.Season, report_stock.RRPGBP,
+report_stock.RRPEuro, report_stock.Avilable_Stock, report_stock.Count,
+report_stock.Collection, report_stock.Article,
+
+customer_report_stock.Lowest_Sold_Price, customer_report_stock.Total_Qty_Sold,
+customer_report_stock.Tiems_Sold, customer_report_stock.Max_Sold_Price,
+customer_report_stock.Customer_Code,
+
+sku.Origin, sku.FabCom, sku.WashCare
+
+FROM 
+	customer_report_stock join report_stock join sku
+
+ON 
+	customer_report_stock.Report_Stock_Id = report_stock.Id
+
+AND 
+	report_stock.SKU_Id = sku.Id
+
+AND
+	report_stock.Avilable_Stock > '10'
+
+/*
+WHERE report_stock.BrandName IN ('YUMI RETAIL','YUMI ORIGINAL', 'YUMI GIRLS RETAIL', 'YUMI GIRLS', 'Yumi Curves',
+								'YUMI ACCESSORIES', 'YUMI' ,'Y by Yumi', 'UTTAM KIDS', 'ISKA', 'YUMI-RETAIL') AND 
+*/
+WHERE report_stock.BrandName IN ('ISKA', 'MELA', 'MELA LONDON', 'Yumi') AND 
+                                
+report_stock.Season IN ('AW10', 'AW11', 'AW12', 'AW13', 'AW14', 'AW15', 
+'AW16', 'AW17', 'AW18', 'AW19','IS10', 'IS11', 
+'IS12', 'IS13', 'IS14', 'IS15', 'IW10', 'IW11', 
+'IW12', 'IW13', 'IW14', 'IW15', 'PD19', 'SS01', 
+'SS02', 'SS10', 'SS11', 'SS12', 'SS13', 'SS14', 
+'SS15', 'SS16', 'SS17', 'SS18', 'SS19', 'SS20','Missing')
+
+GROUP BY LineCol 
+ORDER BY LineCol
+
+/*/////////////////////////////// end   //////////////*/
+
+//////////// OUTER jOIN ////////////////////////////
+
+/* select t1.Product, t1.Quantity, t2.Cost */
+
+SELECT
+
+t1.SKU, t1.LineCol, t1.Style, t1.Colour,
+t1.BrandName, t1.Season, t1.RRPGBP,
+t1.RRPEuro, t1.Avilable_Stock, t1.Count,
+t1.Collection, t1.Article,
+t2.Total_Qty_Sold,
+t2.Lowest_Sold_Price,
+t2.Landed_Cost_GBP,
+t2.Tiems_Sold,
+t2.Customer_Code
+
+/*
+report_stock.SKU, report_stock.LineCol, report_stock.Style, report_stock.Colour,
+report_stock.BrandName, report_stock.Season, report_stock.RRPGBP,
+report_stock.RRPEuro, report_stock.Avilable_Stock, report_stock.Count,
+report_stock.Collection, report_stock.Article,
+customer_report_stock.Total_Qty_Sold
+
+*/
+
+/* from table1 as t1 */
+
+FROM report_stock as t1
+
+/* left join table2 as t2 */
+
+LEFT OUTER JOIN customer_report_stock as t2
+
+    /* on t1.Product = t2.Product */
+
+ON t1.LineCol = t2.LineCol
+
+GROUP BY LineCol 
+ORDER BY LineCol
+
+/*/////////////////////////////// end   //////////////*/
+
+//////////// OUTER JOIN 1.1 ////////////////////////////
+
+SELECT
+t1.SKU, t1.LineCol, t1.Style, t1.Colour,
+t1.BrandName, t1.Season, t1.RRPGBP,
+t1.RRPEuro, t1.Avilable_Stock, t1.Count,
+t1.Collection, t1.Article,
+t2.Total_Qty_Sold,
+t2.Lowest_Sold_Price,
+t2.Landed_Cost_GBP,
+t2.Tiems_Sold,
+t2.Customer_Code,
+t3.FabCom,
+t3.WashCare,
+t3.Origin
+
+FROM report_stock as t1
+
+LEFT OUTER JOIN customer_report_stock as t2
+
+ON (t1.LineCol = t2.LineCol)
+    
+LEFT OUTER JOIN sku as t3
+
+ON (t1.LineCol = t3.LineCol)
+WHERE t1.Avilable_Stock > '2' AND
+
+t1.BrandName IN ('Yumi', 'YUMI RETAIL', 'Y by Yumi', 'YUMI ORIGINAL', 'YUMI-RETAIL') AND
+
+t1.Season IN ('AW11', 'AW12', 'AW13', 'AW14', 'AW15', 'AW16', 'AW17', 'AW18', 'AW19', 'IW15',
+				'PD15' 'PD16', 'PD17', 'PD18', 'PD19',
+                'SS13', 'SS14', 'SS15', 'SS16', 'SS17', 'SS18', 'SS19','SS20')
+
+GROUP BY LineCol 
+ORDER BY LineCol
+
+/*/////////////////////////////// end   //////////////*/
 
 
 
